ca7cebd10d55b87f24684b15de104651adb21b17
Sebastian Kippe <sebastian@kip.pe>
2013-12-21 15:28:52 +0100

    Reorganize visualization code a bit

--- js/app.js
+++ js/app.js
@@ -17,18 +17,10 @@ App = Ember.Application.create({
     var length = total;
     if (length < 2) { length = 2; }
 
-    if (total < 1) {
-      speed = 4;
-    }
-    else if (total < 10) {
-      speed = 3;
-    }
-    else if (total < 50) {
-      speed = 2;
-    }
-    else {
-      speed = 1;
-    }
+    if      (total < 1)  { speed = 4; }
+    else if (total < 10) { speed = 3; }
+    else if (total < 50) { speed = 2; }
+    else                 { speed = 1; }
 
     flakeArray[flakeArray.length] = new Flake({
       width: length,
@@ -72,10 +64,124 @@ App.Transaction = Ember.Object.extend({
   }.property('total')
 });
 
-App.IndexView = Ember.View.extend({
+// Visualization
+
+var canvas = null;
+var context = null;
+var bufferCanvas = null;
+var bufferCanvasCtx = null;
+var flakeArray = [];
+
+// TODO use ember object
+var Flake = function(options) {
+  this.x = Math.round(Math.random() * context.canvas.width);
+  this.y = -10;
+  this.drift = Math.random();
+  this.speed = options.speed || Math.round(Math.random() * 3) + 1;
+  this.width = options.width * 2;
+  this.height = options.height * 2;
+};
+
+// TODO use controller for content
+App.VisualizationController = Ember.ArrayController.extend({
+  content: []
+});
+App.visualizationController = App.VisualizationController.create();
+
+App.VisualizationView = Ember.View.extend({
+  templateName: 'visualization',
+
   didInsertElement: function() {
-    initCanvas();
-  }
+    this.initCanvas();
+    this.draw();
+    setIntervalWithContext(this.animate, 30, this); // TODO requestAnimationFrame?
+  },
+
+  initCanvas: function() {
+    // TODO use ember objects, computed properties for dimensions, etc.
+    canvas = document.getElementById('visualizer');
+    context = canvas.getContext("2d");
+    bufferCanvas = document.createElement("canvas");
+    bufferCanvas.width = canvas.width = window.innerWidth - 40;
+    bufferCanvas.height = canvas.height = window.innerHeight / 3;
+    bufferCanvasCtx = bufferCanvas.getContext("2d");
+  },
+
+  resize: function(mode) {
+    if (mode === 'full') {
+      bufferCanvas.width = canvas.width = window.outerWidth;
+      bufferCanvas.height = canvas.height = window.outerHeight;
+    } else {
+      bufferCanvas.width = canvas.width = window.innerWidth - 40;
+      bufferCanvas.height = canvas.height = window.innerHeight / 3;
+    }
+  },
+
+  animate: function() {
+    this.update();
+    this.draw();
+  },
+
+  blank: function() {
+    bufferCanvasCtx.fillStyle = "rgba(18, 18, 18, 1)";
+    bufferCanvasCtx.fillRect(0, 0, canvas.width, canvas.height);
+  },
+
+  update: function() {
+    for (var i = 0; i < flakeArray.length; i++) {
+      if (flakeArray[i].y < context.canvas.height) {
+        flakeArray[i].y += flakeArray[i].speed;
+        if (flakeArray[i].y > context.canvas.height) {
+          flakeArray[i].y = -5;
+        }
+        flakeArray[i].x += flakeArray[i].drift;
+        if (flakeArray[i].x > context.canvas.width) {
+          flakeArray[i].x = 0;
+        }
+      }
+    }
+  },
+
+  draw: function() {
+    context.save();
+    this.blank();
+    for (var i = 0; i < flakeArray.length; i++) {
+      var length = flakeArray[i].width / 2;
+      var radgrad = bufferCanvasCtx.createRadialGradient(
+        flakeArray[i].x + length,
+        flakeArray[i].y + length,
+        0,
+        flakeArray[i].x + length,
+        flakeArray[i].y + length,
+        length
+      );
+      radgrad.addColorStop(0, '#c90');
+      radgrad.addColorStop(0.99, '#c90');
+      radgrad.addColorStop(1, 'rgba(0,0,0,0)');
+
+      bufferCanvasCtx.fillStyle = radgrad;
+
+      bufferCanvasCtx.fillRect(
+        flakeArray[i].x,
+        flakeArray[i].y,
+        flakeArray[i].width,
+        flakeArray[i].height
+      );
+    }
+    context.drawImage(bufferCanvas, 0, 0, bufferCanvas.width, bufferCanvas.height);
+    context.restore();
+  },
+
+  // click: function(e) {
+  //   if (runPrefixMethod(document, "FullScreen") || runPrefixMethod(document, "IsFullScreen")) {
+  //     runPrefixMethod(document, "CancelFullScreen");
+  //     this.resize('normal');
+  //   }
+  //   else {
+  //     runPrefixMethod(e.target, "RequestFullScreen");
+  //     this.resize('full');
+  //   }
+  // }
 });
 
 // Websocket API
@@ -133,90 +239,36 @@ function fetchGeneralInfo() {
   setTimeout(fetchGeneralInfo, 10000);
 }
 
-// Visualization
+// Utils
 
-var canvas = null;
-var context = null;
-var bufferCanvas = null;
-var bufferCanvasCtx = null;
-var flakeArray = [];
-
-function initCanvas() {
-  canvas = document.getElementById('visualizer');
-  context = canvas.getContext("2d");
-
-  bufferCanvas = document.createElement("canvas");
-  bufferCanvas.width = canvas.width = window.innerWidth - 40;
-  bufferCanvas.height = canvas.height = window.innerHeight / 3;
-  bufferCanvasCtx = bufferCanvas.getContext("2d");
-
-  draw();
-
-  setInterval(animate, 30);
-}
-
-function animate() {
-  update();
-  draw();
-}
-
-function blank() {
-  bufferCanvasCtx.fillStyle = "rgba(18, 18, 18, 1)";
-  bufferCanvasCtx.fillRect(0, 0, canvas.width, canvas.height);
-}
-
-function update() {
-  for (var i = 0; i < flakeArray.length; i++) {
-    if (flakeArray[i].y < context.canvas.height) {
-      flakeArray[i].y += flakeArray[i].speed;
-      if (flakeArray[i].y > context.canvas.height) {
-        flakeArray[i].y = -5;
-      }
-      flakeArray[i].x += flakeArray[i].drift;
-      if (flakeArray[i].x > context.canvas.width) {
-        flakeArray[i].x = 0;
-      }
+var runPrefixMethod = function(obj, method) {
+  var pfx = ["webkit", "moz", "ms", "o", ""];
+  var p = 0, m, t;
+  while (p < pfx.length && !obj[m]) {
+    m = method;
+    if (pfx[p] === "") {
+      m = m.substr(0,1).toLowerCase() + m.substr(1);
     }
+    m = pfx[p] + m;
+    t = typeof obj[m];
+    if (t !== "undefined") {
+      pfx = [pfx[p]];
+      return (t === "function" ? obj[m]() : obj[m]);
+    }
+    p++;
   }
-}
+};
 
-function Flake(options) {
-  this.x = Math.round(Math.random() * context.canvas.width);
-  this.y = -10;
-  this.drift = Math.random();
-  this.speed = options.speed || Math.round(Math.random() * 3) + 1;
-  this.width = options.width;
-  this.height = options.height;
-}
+var setIntervalWithContext = function(code,delay,context){
+  return setInterval(function() {
+    code.call(context);
+  },delay);
+};
 
-function draw() {
-  context.save();
-  blank();
-  for (var i = 0; i < flakeArray.length; i++) {
-    var length = flakeArray[i].width / 2;
-    var radgrad = bufferCanvasCtx.createRadialGradient(
-      flakeArray[i].x + length,
-      flakeArray[i].y + length,
-      0,
-      flakeArray[i].x + length,
-      flakeArray[i].y + length,
-      length
-    );
-    radgrad.addColorStop(0, '#c90');
-    radgrad.addColorStop(0.99, '#c90');
-    radgrad.addColorStop(1, '#121212');
-
-    bufferCanvasCtx.fillStyle = radgrad;
-
-    bufferCanvasCtx.fillRect(
-      flakeArray[i].x,
-      flakeArray[i].y,
-      flakeArray[i].width,
-      flakeArray[i].height
-    );
-  }
-  context.drawImage(bufferCanvas, 0, 0, bufferCanvas.width, bufferCanvas.height);
-  context.restore();
-}
+var setTimeoutWithContext = function(code,delay,context){
+  return setTimeout(function() {
+    code.call(context);
+  },delay);
+};
 
 init();
